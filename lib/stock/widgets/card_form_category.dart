e9fa3422c10b581c667adb6841075b7e7c786ec1
Andrea Cornejo <32601297+andreacornejo@users.noreply.github.com>
2022-06-03 11:57:10 -0400

    Diseño de seccion inventario ## Pantalla de inventario - Card stock - New category en inventario - agregar producto

---
+++ lib/stock/widgets/card_form_category.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import '../../../constans.dart';
+
+enum SingingCharacter { disponible, nodisponible }
+
+class CardFormCategoryStock extends StatefulWidget {
+  const CardFormCategoryStock({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CardFormCategoryStock> createState() => _CardFormCategoryStockState();
+}
+
+class _CardFormCategoryStockState extends State<CardFormCategoryStock> {
+  String dropdownValue = 'Platos';
+  SingingCharacter? _character = SingingCharacter.disponible;
+
+  /// Subtitle Forms
+  Container titleCardForm(String text) {
+    return Container(
+      alignment: Alignment.topLeft,
+      margin: const EdgeInsets.only(right: 5, bottom: 5),
+      child: Text(
+        text,
+        style: textStyleSubtitle,
+      ),
+    );
+  }
+
+  /// Nombre de la Categoría
+  Column nameCategory() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        titleCardForm('Nombre de la Categoría'),
+        Container(
+          margin: const EdgeInsets.only(bottom: 10),
+          child: TextFormField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  /// Descripción
+  Column description() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        titleCardForm('Descripción'),
+        Container(
+          margin: const EdgeInsets.only(bottom: 10),
+          child: TextFormField(
+            maxLines: 3,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  // /// Foto del Producto
+  // Column photoProduct() {
+  //   return Column(
+  //     crossAxisAlignment: CrossAxisAlignment.start,
+  //     children: [
+  //       titleCardForm('Foto del Producto'),
+  //       InkWell(
+  //         child: Container(
+  //             width: MediaQuery.of(context).size.width / 3 * 1.3,
+  //             height: MediaQuery.of(context).size.width / 3 * 1.4,
+  //             margin: const EdgeInsets.only(bottom: 10),
+  //             decoration: BoxDecoration(
+  //               borderRadius: BorderRadius.circular(10),
+  //               border: Border.all(color: secondColor),
+  //             ),
+  //             child: const Icon(Icons.add_photo_alternate_rounded, size: 50)),
+  //       ),
+  //     ],
+  //   );
+  // }
+
+  /// Estado
+  Container status() {
+    return Container(
+      width: MediaQuery.of(context).size.width / 2 * 0.8,
+      margin: const EdgeInsets.only(bottom: 10, top: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          titleCardForm('Estado'),
+          RadioListTile<SingingCharacter>(
+            contentPadding: const EdgeInsets.all(0),
+            visualDensity: const VisualDensity(
+              horizontal: VisualDensity.minimumDensity,
+              vertical: VisualDensity.minimumDensity,
+            ),
+            title: const Text('Disponible'),
+            value: SingingCharacter.disponible,
+            groupValue: _character,
+            onChanged: (SingingCharacter? value) {
+              setState(() {
+                _character = value;
+              });
+            },
+          ),
+          RadioListTile<SingingCharacter>(
+            contentPadding: const EdgeInsets.all(0),
+            visualDensity: const VisualDensity(
+              horizontal: VisualDensity.minimumDensity,
+              vertical: VisualDensity.minimumDensity,
+            ),
+            title: const Text('No Disponible'),
+            value: SingingCharacter.nodisponible,
+            groupValue: _character,
+            onChanged: (SingingCharacter? value) {
+              setState(() {
+                _character = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 15, bottom: 15, left: 10, right: 10),
+      margin: const EdgeInsets.only(bottom: 25, left: 5, right: 5),
+      decoration: boxShadow,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          nameCategory(),
+          description(),
+          status(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/stock/widgets/card_stock.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../../../constans.dart';
+
+class CardStock extends StatelessWidget {
+  final String total;
+  final String nameSale;
+  const CardStock({
+    Key? key,
+    required this.total,
+    required this.nameSale,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      margin: const EdgeInsets.only(bottom: 15),
+      decoration: boxShadow,
+      child: InkWell(
+          onTap: () {
+            // Navigator.of(context).push(
+            //   CupertinoPageRoute(
+            //     builder: (context) => const DetailSaleScreen(
+            //       typeSale: 'purchase',
+            //     ),
+            //   ),
+            // );
+          },
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 2 * 1.2 - 15,
+                    child: Text(
+                      nameSale,
+                      style: textStyleTitleSecond,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 4,
+                    child: Text(
+                      'Bs. ' + total,
+                      style: const TextStyle(
+                          fontFamily: "Work Sans",
+                          fontWeight: FontWeight.w700,
+                          fontSize: fontSizeTitle,
+                          color: redColor),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )),
+    );
+  }
+}
